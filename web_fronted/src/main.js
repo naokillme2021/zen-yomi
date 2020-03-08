045371f6ab5db0302095c6a5fff321a04e001083
jacoloves <5511068t@gmail.comn>
2020-03-08 13:53:46 +0900

    20200308

--- web_fronted/src/main.js
+++ web_fronted/src/main.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import _ from 'lodash';
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
 
 import { makeStyles } from '@material-ui/styles';
 import { theme } from './styles/theme';
 import { Box, Typography, GridList } from '../node_modules/@material-ui/core';
 import Cards from './component/Cards';
 import News from './data/News.json';
+import axios from 'axios';
+import { TEST_NEWS } from './data/api';
+
+axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://n9rsktt3h2.execute-api.us-east-2.amazonaws.com'
+
 
 const useStyles = makeStyles({
     NegaPosiTop: {
@@ -20,7 +26,30 @@ const useStyles = makeStyles({
 });
 
 const NegaPosiNews = () => {
+    axios.defaults.withCredentials = true;
+    const [newsData, setNewsData] = useState();
+
+    const getData = async () => {
+        try {
+            console.log("ここまでは来てる？");
+            const result = await axios.get(
+                `https://n9rsktt3h2.execute-api.us-east-2.amazonaws.com/zen-Yomu-mock/news/`,
+                {
+                    withCredentials: true
+                }
+            );
+            console.log(result);
+        } catch(err) {
+            console.log("error!");
+        }
+    }
+
     const classes = useStyles();
+    useEffect(() => {
+        getData();
+    });
+    
+    console.log(newsData);
 
     return(
         <div className={classes.NegaPosiTop}>
@@ -28,12 +57,11 @@ const NegaPosiNews = () => {
             <NegaBoxPosi />
             <div>
                 <GridList>
-                    {News.map((item) => (
-                        <Cards headl={item.headline} word={item.words} />
-                    ))}
+                    
                 </GridList>
             </div>
             <NegaBoxNega />
+            <button onClick={() => getData()}>get profile!</button>
         </div>
     );
 }
